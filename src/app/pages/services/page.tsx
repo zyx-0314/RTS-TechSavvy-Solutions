fdb2b3244116df54b68f617ace3cff0957abf033
zyx-0314 <ramirezian037@gmail.com>
2024-07-29 00:19:10 +0800

    refactor(services): added images to data structure

--- src/app/pages/services/page.tsx
+++ src/app/pages/services/page.tsx
@@ -4,25 +4,29 @@ import HeaderNav from "@/components/elements/headerNav";
 import { cn } from "@/lib/utils";
 
 const Services = () => {
-  const services: { title: string; content: string; link: string }[] = [
+  const services: { title: string; content: string; img: string; link: string }[] = [
     {
       title: "Custom Software Development",
       content: "Innovate your business with tailor-made software solutions.",
+      img: "/feature/services/Custom Software Development.jpg",
       link: "/pages/services/softwareDevelopment",
     },
     {
       title: "IT Infrastructure Management",
       content: "Optimize and manage your IT infrastructure efficiently.",
+      img: "/feature/services/IT Infrastructure Management.jpg",
       link: "/pages/services/itInfrastructureManagement",
     },
     {
       title: "Cloud Services",
       content: "Leverage the power of the cloud for scalability and flexibility.",
+      img: "/feature/services/Cloud Services.jpg",
       link: "/pages/services/cloud",
     },
     {
-      title: "Cybersecurity Solutions",
+      title: "CyberSecurity Solutions",
       content: "Protect your digital assets with robust cybersecurity measures.",
+      img: "/feature/services/CyberSecurity Solutions.jpg",
       link: "/pages/services/cyberSecuritySolutions",
     }
   ];
