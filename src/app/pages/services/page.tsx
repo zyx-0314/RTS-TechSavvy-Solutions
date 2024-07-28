e2cb45aecf5c9a25fc0b7a1ee60fa3d1fe416908
zyx-0314 <ramirezian037@gmail.com>
2024-07-28 23:02:24 +0800

    refactor(services): apply component in services page

--- src/app/pages/services/page.tsx
+++ src/app/pages/services/page.tsx
@@ -1,32 +1,29 @@
+import ServiceCard from "@/components/elements/card/service";
 import HeaderNav from "@/components/elements/headerNav";
 
 import { cn } from "@/lib/utils";
-
-import style from './services.module.css';
-import Image from "next/image";
-import Link from "next/link";
 
 const Services = () => {
   const services: { title: string; content: string; link: string }[] = [
     {
       title: "Custom Software Development",
       content: "Innovate your business with tailor-made software solutions.",
-      link: "/services/custom-software-development",
+      link: "/pages/services/softwareDevelopment",
     },
     {
       title: "IT Infrastructure Management",
       content: "Optimize and manage your IT infrastructure efficiently.",
-      link: "/services/it-infrastructure-management",
+      link: "/pages/services/itInfrastructureManagement",
     },
     {
       title: "Cloud Services",
       content: "Leverage the power of the cloud for scalability and flexibility.",
-      link: "/services/cloud-services",
+      link: "/pages/services/cloud",
     },
     {
       title: "Cybersecurity Solutions",
       content: "Protect your digital assets with robust cybersecurity measures.",
-      link: "/services/cybersecurity-solutions",
+      link: "/pages/services/cyberSecuritySolutions",
     }
   ];
 
@@ -39,16 +36,10 @@ const Services = () => {
         <div className={cn("cardholder grid grid-cols-2 grid-rows-2 gap-4 py-6")}>
           {
             services.map((content, index) => (
-              <div key={index} className={cn("border flex p-5 gap-4 rounded-md")}>
-                <div className="w-11/12 h-100 relative border-2 rounded-md">
-                  <Image src={`/feature/services/${content.title}.jpg`} fill alt={content.title} className="absolute object-cover" />
-                </div>
-                <div className="flex flex-col gap-4">
-                  <h4 className="text-2xl font-bold ">{content.title}</h4>
-                  <p className="line-clamp-3">{content.content}</p>
-                  <Link href={content.link} className="self-end">Check now</Link>
-                </div>
-              </div>
+              <ServiceCard
+                index={index}
+                content={content}
+              />
             ))
           }
         </div>
